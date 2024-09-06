fb30a6d59e0f708a7f1a2cd8f8552a1682274da3
balanscott <balanscott@outlook.com>
2024-09-06 02:21:57 -0500

    Created anonymize.py, settings.ini with starting params and updated generator_randomexp.py

--- common/generator_randomexp.py
+++ common/generator_randomexp.py
@@ -1,9 +1,9 @@
 import rstr
 
-def generate_regexp(randon_string=None) ->str:
+def generate_regexp(randon_string = None) ->str:
     pattern = r'{regex_string}'
     random_string: str = rstr.rstr(pattern)
     return randon_string
 
 if __name__ == '__main__':
-    generate_regexp()
+    generate_regexp(randon_string='abc')
