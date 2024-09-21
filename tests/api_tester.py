63b28ca6737434227adc817f3262513c1b0f143f
balanscott-healthcaredataguy <balanscott@outlook.com>
2024-09-21 18:36:20 -0500

    Created test, web and api directories core backend and also created core python module per main platform function and also created a base testing python module for apis

---
+++ tests/api_tester.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from unittest import TestCase
+
+# Main Program
+def main():
+    print(f"Start Type: {datetime.now}")
+
+    print("Program Ended")
+
+if __name__ == "__main__":
+    main()
