2aa7bfadd706425bfa47693caca19348c7579d22
Alan Scott <64438147+balanscott@users.noreply.github.com>
2024-09-21 20:12:44 -0400

    Web api testing enhancements (#6)
    * Created test, web and api directories core backend and also created core python module per main platform function and also created a base testing python module for apis
    
    * Updated common functions to consolidate capabilities. Put Hashcode generation, GUIDs, and regular expressions generation
    
    ---------
    
    Co-authored-by: balanscott-healthcaredataguy <balanscott@outlook.com>

---
+++ api/terms.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+# Main Program
+def main():
+    print(f"Start Type: {datetime.now}")
+
+    print("Program Ended")
+
+if __name__ == "__main__":
+    main()
--- common/generator_randomexp.py
+++
@@ -1,9 +0,0 @@
-import rstr
-
-def generate_regexp(randon_string = None) ->str:
-    pattern = r'{regex_string}'
-    random_string: str = rstr.rstr(pattern)
-    return randon_string
-
-if __name__ == '__main__':
-    generate_regexp(randon_string='abc')
