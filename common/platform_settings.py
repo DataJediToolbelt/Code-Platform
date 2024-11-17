3a416ef4d95b94f214e60f400c2113e0d2f46b68
Alan Scott <64438147+balanscott@users.noreply.github.com>
2024-11-17 14:23:23 -0600

    Platform configuration mgmt (#19)
    * cleaned up main.py and corrected variable issues, renamed dataclasses directory to data_classes directory and put base configuration code that connects to and retrieves settings and populates variables based on the data. Also, added in SQLite local DB instance of complete data tier to help ease testing process.
    
    * Enabled configuration database connectivity and initial pull back of settings to populate platform variables.
    
    ---------
    
    Co-authored-by: balanscott <balanscott@outlook.com>

--- common/platform_settings.py
+++ common/platform_settings.py
@@ -1,15 +1,20 @@
 # https://docs.python.org/3/library/sqlite3.html
 import sqlite3
+import data_classes.configurations
+from dataclasses import dataclass
 
-def connect_config():
-    con = sqlite3.connect("/platform_data_local/configuration.db")
+def connect_config(db_location):
+    con = sqlite3.connect(db_location+"configuration.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM configuration_details")
     result= cur.fetchall()
     # loop through the rows
     for row in result:
-        print(row)
-        print("\n")
+        # rows = (
+        #    data_classes.configurations.configuration_details(row[0], row[1])
+        #)
+        rowDetails = data_classes.configurations.configuration_details(row[0],row[1])
+    return(result)
 
 if __name__ == "__main__":
     connect_config()
--- main.py
+++ main.py
@@ -1,58 +1,62 @@
 # Python Imports
 from datetime import datetime
 import os
+import connectors.rdbms.sqlite
+#import connectors.rdbms.postgresql
+import connectors.rdbms.sqlserver
 # Platform Imports
 from common.platform_settings import connect_config
-from pathlib import Path
 
-# Local Variables
-local_path = os.getcwd()
-local_database_path = local_path + os.sep + "platform_data_local"+ os.sep
-
-start_time = datetime.now()
-# Auditing Variables
-component_name = "platform_processor"
-processing_run_datetime = datetime.now()
-processing_objectname = ""
-operation_name = None
-
-"""
-Calculating Tasks
-a = datetime.datetime.now()
-b = datetime.datetime.now()
-c = b - a
-# c.days, c.seconds
-print(f"{c.seconds}")
-print(f"{c.microseconds}")
-"""
-# Read Configuration and populate key values to process along
-# This step also has settings to determine if there is a logging output
-# for those running the code
-#config = platform_configuration()
-#platform_operation = config['General']['platform_operation']
-
-# Create a dictionary of base platform queries to enable us to leverage a parameter
-# model for retrieval
-my_query_dict = {'refdata_status': 'select * from refdata_statuses',
-           'datatier': 'select * from datatier',
-           'datatier_structures':'select * from datatier_structures'}
-key_to_find = 'refdata_status'
-
-# Main Program
 def main():
+    # Auditing Variables
+    start_time = datetime.now()
+    component_name = "platform_processor"
+    processing_run_datetime = datetime.now()
+    processing_objectname = ""
+    operation_name = None
+    output_settings = None
+    auditing = True
+    datatier_technologies = None
+    platform_datatier = None
+    # Local Variables
+    local_path = os.getcwd()
+    local_database_path = local_path + os.sep + "platform_data_local" + os.sep
+    referenceapp_guid = None
+    organization_guid = None
     print(f"Data Jedi ToolBelt Platform Started at {datetime.now()}")
-    #print(f"Platform Operation Type: {platform_operation}")
-
-    # Pull in configuration as step1
-    connect_config()
-
-    # Future potential usage
-    #if key_to_find in my_query_dict:
-    #    print(f"Key '{key_to_find}' found with value: {my_query_dict[key_to_find]}")
-    #else:
-    #    print(f"Key '{key_to_find}' not found.")
-
+    # Pull in configuration data from configuration database
+    configuration_details = connect_config(local_database_path)
+    # populate variables to use in platform
+    for list_dtl in configuration_details:
+        for split_list in list_dtl:
+            print(split_list)
+            if (split_list == 'output_settings'):
+                output_settings = list_dtl[2]
+            if (split_list == 'platform_operation'):
+                operation_name = list_dtl[2]
+            if (split_list == 'auditing'):
+                auditing = list_dtl[2]
+            if (split_list == 'datatier'):
+                datatier_technologies = list_dtl[2]
+            if (split_list == 'platform_datatier'):
+                platform_datatier = list_dtl[2]
+            if (split_list == 'referenceapp_guid'):
+                referenceapp_guid = list_dtl[2]
+            if (split_list == 'organization_guid'):
+                organization_guid = list_dtl[2]
+    # Evaluating Need - Connect to platform datatier
+    # sql_connection = create_datatier_connectivity(datatier_technologies, local_database_path)
+    # Build out component that drives data from datatier based on platform_operation
+    # for platform_operation of syntheticdata_generation we need to leverage the platform_datageneration
+    # Table for all records with status = 1 (active)
     print("Program Ended")
+
+def create_datatier_connectivity(datatier_technologies, local_database_path=None):
+    if datatier_technologies == 'sqlite':
+        sql_connection = connectors.rdbms.sqlite.connect(local_database_path)
+    if datatier_technologies == 'sqlserver':
+        sql_connection = connectors.rdbms.sqlserver.create_conn()
+    
 
 if __name__ == "__main__":
     main()
