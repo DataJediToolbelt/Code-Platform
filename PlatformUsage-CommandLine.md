66be47b7aad4bc4d989df2fd2082c2268b2eaa3e
balanscott <balanscott@outlook.com>
2024-11-21 08:09:35 -0600

    Added connector for hl7_client, established dataclasses and method to invoke platform variables and platform settings. Also, enhanced main.py to leverage the platform_variables and platform settings. Updated markdown files as well

--- PlatformUsage-CommandLine.md
+++ PlatformUsage-CommandLine.md
@@ -5,29 +5,17 @@ The following are key technical capabilities the platform provides. The platform
 is intended to have two specific application types it supports with its core codebase,
 an application (main.py) and web tier for web and apis (app.py).
 
-
-### Application
-1. Platform startup, main.py
-2. Settings are loaded and the key setting that is evaluated is platform_operation as
-this determines the action the platform will take. For generating data is data-anon
-This will invoke the method(s) that create guids and/or sha-512 hash codes to be used
-as key reference for input data so no native values are stored during this process.
-3. The keyed data hashes are then reviewed against the platform and either a related 
-dataset is retrieved or a dataset linkage in created. This linkage will be permanent. 
-4. Upserting (inserting) data into the appropriate table(s) and auditing table.
-
 # Application
+The platform operates on the settings configured. While all the settings are important there
+are several that will need to configured for users needs and environments. The following 
+settings are critical: datatier, platform_datatier, database_settings, and platform_operations
+
 1. Platform startup, main.py
-2. Settings are loaded and the key setting that is evaluated is platform_operation as
-this determines the action the platform will take. For generating data is data-generation.
-This will invoke the method(s) that query the relevant platform_ tables.
-3. The appropriate data generation methods are invoked 
-4. Upserting (inserting) data into the datatier_ table(s) and auditing table.
+2. Settings are loaded and the key setting that is evaluated is platform_operation. This setting
+determines the action of the platform. To help everyone with the options we have built a table
+there is a table in the SQLLite database named pltaform_operations which explains the command 
+line options and what they provide.
 
-## Generating Data 
-Generating data within the platform is how the platform grows its data. This is known 
-as the datatier subsystem of the platform.
-
-## Viewing Platform Data and Managing Platform 
+## Generating Synthetic Data 
 Generating data within the platform is how the platform grows its data. This is known 
 as the datatier subsystem of the platform.
--- main.py
+++ main.py
@@ -3,48 +3,19 @@ from datetime import datetime
 import os
 import connectors.rdbms.sqlite
 #import connectors.rdbms.postgresql
-import connectors.rdbms.sqlserver
+#import connectors.rdbms.sqlserver
 # Platform Imports
-from common.platform_settings import connect_config
+import common.platform_settings
+from common.platform_settings import build_platform_variables
+from common.platform_settings import build_platform_config
 
 def main():
-    # Auditing Variables
-    start_time = datetime.now()
-    component_name = "platform_processor"
-    processing_run_datetime = datetime.now()
-    processing_objectname = ""
-    operation_name = None
-    output_settings = None
-    auditing = True
-    datatier_technologies = None
-    platform_datatier = None
-    # Local Variables
-    local_path = os.getcwd()
-    local_database_path = local_path + os.sep + "platform_data_local" + os.sep
-    referenceapp_guid = None
-    organization_guid = None
-    print(f"Data Jedi ToolBelt Platform Started at {datetime.now()}")
-    # Pull in configuration data from configuration database
-    configuration_details = connect_config(local_database_path)
-    # populate variables to use in platform
-    for list_dtl in configuration_details:
-        for split_list in list_dtl:
-            print(split_list)
-            if (split_list == 'output_settings'):
-                output_settings = list_dtl[2]
-            if (split_list == 'platform_operation'):
-                operation_name = list_dtl[2]
-            if (split_list == 'auditing'):
-                auditing = list_dtl[2]
-            if (split_list == 'datatier'):
-                datatier_technologies = list_dtl[2]
-            if (split_list == 'platform_datatier'):
-                platform_datatier = list_dtl[2]
-            if (split_list == 'referenceapp_guid'):
-                referenceapp_guid = list_dtl[2]
-            if (split_list == 'organization_guid'):
-                organization_guid = list_dtl[2]
+    # Set Platform Variables
+    platform_vars = build_platform_variables();
+    # Pull in platform configuration settings from configuration database
+    platform_settings = build_platform_config(platform_vars.local_database_path);
     # Evaluating Need - Connect to platform datatier
+    print("Next Step is modules")
     # sql_connection = create_datatier_connectivity(datatier_technologies, local_database_path)
     # Build out component that drives data from datatier based on platform_operation
     # for platform_operation of syntheticdata_generation we need to leverage the platform_datageneration
