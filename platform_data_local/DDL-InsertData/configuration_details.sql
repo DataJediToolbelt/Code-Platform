6836d327c55b64d7665efad4117cfc168e638aaa
balanscott <balanscott@outlook.com>
2024-11-30 11:01:52 -0600

    Added additional support for local databases needs including a directory within the platform_data_local directory it includes the local database ddl and database load scripts. Also changed the local database name  to platform.db

---
+++ platform_data_local/DDL-InsertData/configuration_details.sql
@@ -0,0 +1,7 @@
+INSERT INTO configuration_details (config_param_name, config_param_type, config_detail) VALUES ('output_settings', 'setting', 'False');
+INSERT INTO configuration_details (config_param_name, config_param_type, config_detail) VALUES ('platform_operation', 'setting', 'syntheticdata_generation');
+INSERT INTO configuration_details (config_param_name, config_param_type, config_detail) VALUES ('datatier', 'setting', 'sqlite');
+INSERT INTO configuration_details (config_param_name, config_param_type, config_detail) VALUES ('platform_datatier', 'setting', 'localhost:5432:datajeditoolbelt');
+INSERT INTO configuration_details (config_param_name, config_param_type, config_detail) VALUES ('auditing', 'setting', 'False');
+INSERT INTO configuration_details (config_param_name, config_param_type, config_detail) VALUES ('referenceapp_guid', 'setting', '16a17a48-d09b-11ea-9579-bba61161b25a');
+INSERT INTO configuration_details (config_param_name, config_param_type, config_detail) VALUES ('organization_guid', 'setting', 'daf0ace4-d09a-11ea-9579-bba61161b25a');
