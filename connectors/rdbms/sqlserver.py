089d89a55758d6d2d616063b6979b0b1b58f2afc
balanscott <balanscott@outlook.com>
2024-12-01 04:56:44 -0600

    Working on basic SQL Server support

--- connectors/rdbms/sqlserver.py
+++ connectors/rdbms/sqlserver.py
@@ -1,5 +1,6 @@
 import pymssql
 from pymssql import Error
+import pypyodbc
 from datetime import time, datetime
 
 # https://learn.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver16
@@ -10,6 +11,25 @@ from datetime import time, datetime
 # https://pypi.org/project/pymssql/
 # # https://learn.microsoft.com/en-us/sql/connect/python/pymssql/step-1-configure-development-environment-for-pymssql-python-development?view=sql-server-ver16
 # # https://learn.microsoft.com/en-us/sql/connect/python/pymssql/step-3-proof-of-concept-connecting-to-sql-using-pymssql?view=sql-server-ver16#connect-and-query-data
+
+def pyodbc_msssql_rdbms_connection(connection_string):
+    try:
+
+        connString = connection_string.split(':')
+        hostname = connString[2].split('@')[1]
+        uid = connString[1].replace('/', '')
+        pwd = connString[2].split('@')[0]
+        dbname = connString[3].split('/')[1]
+
+        connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={hostname};DATABASE={dbname};UID={uid};PWD={pwd}'
+
+        rdbms_connection = pypyodbc.connect(connectionString)
+        print("You are connected to MS SQL Server ")
+    except (Exception, Error) as error:
+        print("Error while connecting to MS SQL Server", error)
+    finally:
+        return rdbms_connection
+
 
 def create_msssql_rdbms_connection(connection_string):
     try:
@@ -38,4 +58,4 @@ def create_msssql_rdbms_connection(connection_string):
 
 if __name__ == "__main__":
     connection_string = "mssql://sa:Developer123@192.168.1.56:1433/New_DataJediToolbelt"
-    create_msssql_rdbms_connection(connection_string)
+    pyodbc_msssql_rdbms_connection(connection_string)
