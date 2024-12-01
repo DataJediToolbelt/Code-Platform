a63b15e48826c015bd7c59429c65d000a0e2348f
balanscott <balanscott@outlook.com>
2024-12-01 14:58:46 -0600

    Resolved a hard coded misspelled value in platform_variables

--- common/platform_settings.py
+++ common/platform_settings.py
@@ -13,7 +13,7 @@ from data_classes.platform import platform_settings
 def build_platform_variables() -> platform_variables:
     # Set Platform Variables
     platform_vars = data_classes.platform.platform_variables(start_time=datetime.now(),
-                                                             component_name="platform_startupr",
+                                                             component_name="platform_startup",
                                                              processing_run_datetime=datetime.now(),
                                                              processing_objectname=None,
                                                              local_path=os.getcwd(),
@@ -77,7 +77,7 @@ def build_platform_config(local_database_path)->platform_settings:
     return platform_settings
 
 def str_to_bool(s):
-    return {"true": True, "false": False,"False":False}.get(s.lower(), False)
+    return {"true": True, "false": False,"False":False,"T":True,"F":False}.get(s.lower(), False)
 
 if __name__ == "__main__":
     build_platform_config()
