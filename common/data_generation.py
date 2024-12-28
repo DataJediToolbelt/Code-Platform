5ca50aa3c5bd65596dc0f37a1c3596ab8ad7812e
balanscott <balanscott@outlook.com>
2024-12-28 00:25:00 -0600

    Updates to core project to finalize several activities that are data and platform related.

--- common/data_generation.py
+++ common/data_generation.py
@@ -10,7 +10,9 @@ def generate_regexp(random_string) ->str:
     return random_string
 
 if __name__ == '__main__':
-    random_string ='^4[0-9]{12}(?:[0-9]{3})?$'
+    #random_string ='^4[0-9]{12}(?:[0-9]{3})?$'
+    #random_string ='^(?!(000|666|9))\d{3}-(?!00)\d{2}-(?!0000)\d{4}$|^(?!(000|666|9))\d{3}(?!00)/\d{2}(?!0000)\d{4}$'
+    random_string ='^(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$'
     returned_data = generate_regexp(random_string)
 
     print(f"Random String: {returned_data}")
